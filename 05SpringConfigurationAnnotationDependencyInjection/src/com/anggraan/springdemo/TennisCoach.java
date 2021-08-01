9ed46330dc7b6658d393676bb2c03d6a53c06fd5
Meursaultt <anggraalheranasmitautomo@gmail.com>
2021-08-01 20:15:41 +0700

    Menambahkan qualifier sehingga hanya qualifier saja yang di injeksi

--- 05SpringConfigurationAnnotationDependencyInjection/src/com/anggraan/springdemo/TennisCoach.java
+++ 05SpringConfigurationAnnotationDependencyInjection/src/com/anggraan/springdemo/TennisCoach.java
@@ -1,6 +1,7 @@
 package com.anggraan.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,7 @@ public class TennisCoach implements Coach
 {
     // field injection otomatis dengan teknologi java reflection
     @Autowired
+    @Qualifier("happyFortuneService")
     private FortuneService fortuneService;
 
     // define a default constructor
