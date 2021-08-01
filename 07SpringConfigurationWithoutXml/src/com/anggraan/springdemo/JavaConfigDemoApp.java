ef9007f5db403ddddbd7d3f593e5fef8d210ae8a
Meursaultt <anggraalheranasmitautomo@gmail.com>
2021-08-01 22:00:17 +0700

    Menambah file baru

--- 07SpringConfigurationWithoutXml/src/com/anggraan/springdemo/JavaConfigDemoApp.java
+++ 07SpringConfigurationWithoutXml/src/com/anggraan/springdemo/JavaConfigDemoApp.java
@@ -6,8 +6,18 @@ public class JavaConfigDemoApp
 {
     public static void main(String[] args)
     {
-        // read spring config class
+        // read spring config java class
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(SportConfig.class);
+
+        // get the bean from spring container
+        Coach coach = context.getBean("tennisCoach", Coach.class);
+
+        // call a method on the bean
+        System.out.println(coach.getDailyFortune());
+        System.out.println(coach.getDailyWorkout());
+
+        // close the context
+        context.close();
     }
 }
---
+++ 07SpringConfigurationWithoutXml/src/com/anggraan/springdemo/TennisCoach.java
@@ -0,0 +1,74 @@
+package com.anggraan.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class TennisCoach implements Coach
+{
+    // field injection otomatis dengan teknologi java reflection
+    @Autowired
+    @Qualifier("fileFortuneService")
+    private FortuneService fortuneService;
+
+    // define a default constructor
+    public TennisCoach()
+    {
+        System.out.println(">> TennisCoach inside default constructor");
+    }
+
+    /*
+    @Autowired
+    public TennisCoach(FortuneService fortuneService)
+    {
+        this.fortuneService = fortuneService;
+    }
+    */
+
+/*    @Autowired
+    public void setFortuneService(FortuneService fortuneService)
+    {
+        System.out.println(">> TennisCoach inside setFortuneService() method");
+        this.fortuneService = fortuneService;
+    }*/
+
+/*
+    @Autowired
+    public void doSomeCrazyStuff(FortuneService fortuneService)
+    {
+        System.out.println(">> TennisCoach inside setFortuneService() method");
+        this.fortuneService = fortuneService;
+    }
+*/
+
+    @Override
+    public String getDailyWorkout()
+    {
+        return "Practice your backhand volley";
+    }
+
+    @Override
+    public String getDailyFortune()
+    {
+        return fortuneService.getFortune();
+    }
+
+    // define my init method
+    @PostConstruct
+    public void doMyStartupStuff()
+    {
+        System.out.println(">> tennisCoach: inside of doMytartupStuff");
+    }
+
+    // define my destroy method
+    @PreDestroy
+    public void doMyCleanupStuff()
+    {
+        System.out.println(">> tennisCoach: inside of doMyCleanupStuff");
+    }
+}
