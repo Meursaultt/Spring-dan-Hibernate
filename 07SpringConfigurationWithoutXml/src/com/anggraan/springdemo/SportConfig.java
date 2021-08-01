8b4502e214487bb5cdd0534c3bec7d8384ea9479
Meursaultt <anggraalheranasmitautomo@gmail.com>
2021-08-01 21:52:20 +0700

    Membuat folder baru

---
+++ 07SpringConfigurationWithoutXml/src/com/anggraan/springdemo/SportConfig.java
@@ -0,0 +1,11 @@
+package com.anggraan.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.anggraan.springdemo")
+public class SportConfig
+{
+
+}
