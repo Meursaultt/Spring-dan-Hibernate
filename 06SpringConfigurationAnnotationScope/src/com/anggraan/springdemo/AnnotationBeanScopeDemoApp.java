3bcf2b2e6041004905448f15a35038839e683849
Meursaultt <anggraalheranasmitautomo@gmail.com>
2021-08-01 21:15:44 +0700

    Scope

---
+++ 06SpringConfigurationAnnotationScope/src/com/anggraan/springdemo/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,15 @@
+package com.anggraan.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp
+{
+    public static void main(String[] args)
+    {
+        // load spring config file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // retrieve bean from spring container
+    }
+}
---
+++ 06SpringConfigurationAnnotationScope/src/com/anggraan/springdemo/TennisCoach.java
@@ -0,0 +1,56 @@
+package com.anggraan.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach
+{
+    // field injection otomatis dengan teknologi java reflection
+    @Autowired
+    @Qualifier("randomFortuneService")
+    private FortuneService fortuneService;
+
+    // define a default constructor
+    public TennisCoach()
+    {
+        System.out.println(">> TennisCoach inside default constructor");
+    }
+
+    /*
+    @Autowired
+    public TennisCoach(FortuneService fortuneService)
+    {
+        this.fortuneService = fortuneService;
+    }
+    */
+
+/*    @Autowired
+    public void setFortuneService(FortuneService fortuneService)
+    {
+        System.out.println(">> TennisCoach inside setFortuneService() method");
+        this.fortuneService = fortuneService;
+    }*/
+
+/*
+    @Autowired
+    public void doSomeCrazyStuff(FortuneService fortuneService)
+    {
+        System.out.println(">> TennisCoach inside setFortuneService() method");
+        this.fortuneService = fortuneService;
+    }
+*/
+
+    @Override
+    public String getDailyWorkout()
+    {
+        return "Practice your backhand volley";
+    }
+
+    @Override
+    public String getDailyFortune()
+    {
+        return fortuneService.getFortune();
+    }
+}
